1c7a06a71c1ed44d05b5b6f4ae778ce5cc9efc05
Akam Programist <akadev404@gmail.com>
2022-11-01 03:30:49 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+</script>
+<template>
+  <Navbar />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<style scoped></style>
