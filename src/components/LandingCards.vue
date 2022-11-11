fcf582ea482a62d87f30d4812a84bbef14a3a234
Akam Programist <akadev404@gmail.com>
2022-11-11 20:51:35 +0300

    commits

--- src/components/LandingCards.vue
+++ src/components/LandingCards.vue
@@ -1,21 +1,6 @@
 <template>
-  <div class="text-white mx-8 my-5 w-auto bg-grey p-10 duration-500 hover:bg-redish">
-    <p>icon</p>
-    <h1>future vision</h1>
-    <p>
-      It is a long established fact that a reader will be distracted by the
-      readable content of a page.
-    </p>
-  </div>
-  <div class="text-white mx-8 my-5 w-auto bg-grey p-10 duration-500 hover:bg-redish">
-    <p>icon</p>
-    <h1>future vision</h1>
-    <p>
-      It is a long established fact that a reader will be distracted by the
-      readable content of a page.
-    </p>
-  </div>
-  <div class="text-white mx-8 my-5 w-auto bg-grey p-10 duration-500 hover:bg-redish">
+
+  <div v-for="i in 3" class="text-white mx-8 my-5 w-auto bg-grey p-10 duration-500 hover:bg-redish">
     <p>icon</p>
     <h1>future vision</h1>
     <p>
