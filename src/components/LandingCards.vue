545d5a4472760d26d597964bc121e91d4cf466d4
Akam Programist <akadev404@gmail.com>
2022-11-14 22:29:51 +0300

    commits

--- src/components/LandingCards.vue
+++ src/components/LandingCards.vue
@@ -1,27 +1,29 @@
 <template>
-  <div
-    v-for="i in 3"
-    class="text-white mx-8 my-5 w-auto bg-grey p-10 space-y-5 duration-500 hover:bg-redish hover:text-white"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="{1.5}"
-      stroke="currentColor"
-      class="W-16 h-16"
+  <div class="mb-16">
+    <div
+      v-for="i in 3"
+      class="text-white mx-8 my-10 w-auto bg-grey p-10 space-y-5 duration-500 hover:bg-redish hover:text-white"
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-      />
-    </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="{1.5}"
+        stroke="currentColor"
+        class="W-16 h-16"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+        />
+      </svg>
 
-    <h1 class="text-white text-2xl font-extrabold">future vision</h1>
-    <p>
-      It is a long established fact that a reader will be distracted by the
-      readable content of a page.
-    </p>
+      <h1 class="text-white text-2xl font-extrabold">future vision</h1>
+      <p>
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page.
+      </p>
+    </div>
   </div>
 </template>
