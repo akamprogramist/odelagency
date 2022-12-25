96b6c78cf38097ba8da81d0bf812d61407f62a55
Akam Programist <akadev404@gmail.com>
2022-12-25 17:32:56 +0300

    commits

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,126 +4,27 @@
     <p class="text-redish mx-8 font-lightbold">
       WE'RE MORE THAN A DIGITAL AGENCY
     </p>
+    <p class="mx-8">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore corrupti
+      temporibus fuga earum asperiores, alias excepturi sit. Impedit, fugit
+      laudantium.
+    </p>
   </div>
-  <div class="text-white mx-8 my-16 space-y-8">
+  <div class="relative px-8 my-8 hover:opacity-70" v-for="i in 3">
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+    />
+    <div class="absolute text-white bottom-5 left-16">
+      <h3 class="text-2xl font-extrabold">Top Left</h3>
+      <p class="text-lg">be better with us</p>
+    </div>
+  </div>
+  <div v-for="i in 3" class="text-white mx-8 my-16 space-y-5">
     <p class="font-bold text-2xl">Who we are</p>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore corrupti
       temporibus fuga earum asperiores, alias excepturi sit. Impedit, fugit
       laudantium.
     </p>
-
-    <div id="default-carousel" class="relative" data-carousel="static">
-      <!-- Carousel wrapper -->
-      <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-        <!-- Item 1 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <span
-            class="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800"
-            >First Slide</span
-          >
-          <img
-            src="../assets/Blue Modern Just Listed Real Estate Instagram Post.png"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 hover:opacity-70 duration-300"
-            alt="..."
-          />
-        </div>
-        <!-- Item 2 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="../assets/Blue Modern Just Listed Real Estate Instagram Post (2).png"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 hover:opacity-70 duration-300"
-            alt="..."
-          />
-        </div>
-        <!-- Item 3 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="../assets/Blue Modern Just Listed Real Estate Instagram Post (1).png"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 hover:opacity-70 duration-300"
-            alt="..."
-          />
-        </div>
-      </div>
-      <!-- Slider indicators -->
-      <div
-        class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2"
-      >
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 1"
-          data-carousel-slide-to="0"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 2"
-          data-carousel-slide-to="1"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 3"
-          data-carousel-slide-to="2"
-        ></button>
-      </div>
-      <!-- Slider controls -->
-      <button
-        type="button"
-        class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-prev
-      >
-        <span
-          class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
-        >
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 19l-7-7 7-7"
-            ></path>
-          </svg>
-          <span class="sr-only">Previous</span>
-        </span>
-      </button>
-      <button
-        type="button"
-        class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-next
-      >
-        <span
-          class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
-        >
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M9 5l7 7-7 7"
-            ></path>
-          </svg>
-          <span class="sr-only">Next</span>
-        </span>
-      </button>
-    </div>
   </div>
 </template>
